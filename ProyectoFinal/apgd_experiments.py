4b414b1b39824a8f673518579eb2967d663280f5
Camilo M <camilo.marino.cabrera@gmail.com>
2020-12-21 00:16:04 -0300

    Se continua con experimentos de apgd

--- ProyectoFinal/apgd_experiments.py
+++ ProyectoFinal/apgd_experiments.py
@@ -7,7 +7,6 @@ Created on Sun Dec 20 00:39:07 2020
 """
 
 import numpy as np
-from time import time
 from tqdm import tqdm
 import torch
 device = 'cuda'
@@ -16,27 +15,31 @@ from torch import optim
 
 #%%
 
-X = np.load('X.npy')#[:, 0]
+X = np.load('X.npy')
 D = np.load('D.npy')
 A_opt = np.load('A_opt.npy')
-#%%
 X = torch.tensor(X, device=device, dtype=torch.float32)
 D = torch.tensor(D, device=device, dtype=torch.float32)
 A_opt = torch.tensor(A_opt, device=device, dtype=torch.float32)
+
+#%%
 A = torch.zeros((D.shape[1], X.shape[1]), device=device, dtype=torch.float32, requires_grad=True)
 p = D.T@X
 Q = D.T@D
-lr = 5e-3#100/torch.norm(Q)
-
-optimizer = optim.Adam([A], lr=lr)
+lr = 10e-3#100/torch.norm(Q)
+#%%
+optimizer = optim.Adam([A], lr=lr, betas=(0.99, 0.999))
 f = lambda A: torch.norm(X-D@A)
 #%%
-for _ in range(100000):
+MAX_ITER = 100_000
+for k in range(MAX_ITER):
     optimizer.zero_grad()
     loss = f(A)
     loss.backward()
     optimizer.step()
-    print(float(loss))
+    if k%1000==0:
+        print(f'{k}\t', float(loss), '\t')
+
     with torch.no_grad():
         A.clip_(0, None)
 
@@ -49,3 +52,10 @@ plt.figure()
 plt.plot(to_np(A[:,0]), label='Adam-PGD')
 plt.plot(to_np(A_opt), label='NNLS', alpha=0.5)
 plt.legend()
+
+#%%
+cost_total = 0
+for i, (x, a) in enumerate(zip(X.T, A.T)):
+    cost = torch.norm(x-D@a)
+    print(f'[{i}] Costo: {cost}')
+    cost_total += cost
